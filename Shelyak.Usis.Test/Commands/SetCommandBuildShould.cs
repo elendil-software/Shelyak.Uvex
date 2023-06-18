c46b62a847bb13bbbfd52fb980dce56ba715932e
Julien Tschäppät <git@elendil.software>
2023-06-18 19:29:03 +0200

    Add unit tests

---
+++ Shelyak.Usis.Test/Commands/SetCommandBuildShould.cs
@@ -0,0 +1,16 @@
+﻿using Shelyak.Usis.Commands;
+using Shelyak.Usis.Enums;
+
+namespace Shelyak.Isis.Test.Commands;
+
+public class SetCommandBuildShould
+{
+    [Theory]
+    [InlineData(DeviceProperty.GRATING_ANGLE, PropertyAttributeType.VALUE, 1.5, "SET;GRATING_ANGLE;VALUE;1.5")]
+    public void ReturnExpectedCommand(DeviceProperty deviceProperty, PropertyAttributeType attributeType, float value, string expectedCommand)
+    {
+        var command = new SetCommand<float>(deviceProperty, attributeType, value);
+        string actual = command.Build();
+        Assert.Equal(expectedCommand, actual);
+    }
+}
