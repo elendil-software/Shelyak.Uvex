f54e19b6e686b4d273e1bef1b67a77071ac7b904
Julien Tschäppät <git@elendil.software>
2023-11-05 17:59:34 +0100

    Fix serialization of float values

--- Shelyak.Usis/Commands/SetCommand.cs
+++ Shelyak.Usis/Commands/SetCommand.cs
@@ -1,4 +1,6 @@
-﻿using Shelyak.Usis.Enums;
+﻿using System;
+using System.Globalization;
+using Shelyak.Usis.Enums;
 
 namespace Shelyak.Usis.Commands
 {
@@ -17,7 +19,22 @@ namespace Shelyak.Usis.Commands
 
         public string Build()
         {
-            return $"{CommandType.SET};{_deviceProperty};{_attributeType};{_value}";
+            return $"{CommandType.SET};{_deviceProperty};{_attributeType};{SerializeValue(_value)}";
         }
+        
+        private string SerializeValue(T value)
+        {
+            switch (value)
+            {
+                case float floatValue:
+                    return floatValue.ToString(CultureInfo.InvariantCulture);
+                case int intValue:
+                    return intValue.ToString();
+                case string stringValue:
+                    return stringValue;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "This type is not supported");
+            }
+        }   
     }
 }
