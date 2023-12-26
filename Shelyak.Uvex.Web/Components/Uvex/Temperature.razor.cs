0fcaa1c4c1139a8688382c690a813055540bc360
Julien Tschäppät <git@elendil.software>
2023-12-26 19:45:03 +0100

    Add Temperature component

---
+++ Shelyak.Uvex.Web/Components/Uvex/Temperature.razor.cs
@@ -0,0 +1,20 @@
+﻿using Shelyak.Uvex.Alpaca;
+using Shelyak.Uvex.Web.HttpClients;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Shelyak.Uvex.Web.Components.Uvex;
+
+public partial class Temperature
+{
+    [Inject]
+    private IUvexHttpClient UvexHttpClient { get; set; }
+    
+    public float CurrentTemperature { get; set; }
+  
+    protected override async Task OnInitializedAsync()
+    {
+        CurrentTemperature = (await UvexHttpClient.GetTemperature()).Value.Value;
+        await base.OnInitializedAsync();
+    }
+}
