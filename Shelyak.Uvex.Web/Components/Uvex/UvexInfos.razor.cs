eb73f1e7de0ccdf2fa84580b4c0c239691e16113
Julien Tschäppät <git@elendil.software>
2023-12-26 19:44:14 +0100

    Add UvexInfos component

---
+++ Shelyak.Uvex.Web/Components/Uvex/UvexInfos.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shelyak.Uvex.Web.HttpClients;
+
+namespace Shelyak.Uvex.Web.Components.Uvex;
+
+public partial class UvexInfos
+{
+    [Inject]
+    private IUvexHttpClient UvexHttpClient { get; set; }
+    
+    
+    public string SpectrographName { get; set; }
+    public string Firmware { get; set; }
+    public string UsisProtocolVersion { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        SpectrographName = (await UvexHttpClient.GetDeviceName()).Value.Value;
+        Firmware = (await UvexHttpClient.GetSoftwareVersion()).Value.Value;
+        UsisProtocolVersion = (await UvexHttpClient.GetProtocolVersion()).Value.Value;
+        
+        await base.OnInitializedAsync();
+    }
+}
