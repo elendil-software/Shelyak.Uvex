18a239ff7e9cca6c662fd95a18bc95a3d8957dce
Julien Tschäppät <git@elendil.software>
2024-06-01 09:54:20 +0200

    Add Swagger configuration component

---
+++ Shelyak.Uvex.Web/Components/Configuration/UpdateSwaggerConfigurationCommand.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.Result;
+using FastEndpoints;
+using Shelyak.Uvex.Web.Components.Shared.Commands;
+using Shelyak.Uvex.Web.Endpoints.Config.Swagger;
+
+namespace Shelyak.Uvex.Web.Components.Configuration;
+
+public record UpdateSwaggerConfigurationCommand(bool Enabled) : ICommand<Result<EmptyResult>>
+{
+    public class Handler : CommandHandlerBase<UpdateSwaggerConfigurationCommand, EmptyResult>
+    {
+        public Handler(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+        }
+        
+        protected override async Task<HttpResponseMessage> ExecuteHttpRequest(UpdateSwaggerConfigurationCommand command, CancellationToken ct)
+        {
+            return await HttpClient.PutAsJsonAsync(UpdateSwaggerRequest.Route, new UpdateSwaggerRequest(command.Enabled), ct);
+        }
+
+        protected override Task<Result> BuildSuccessResponse(HttpResponseMessage response, CancellationToken ct)
+        {
+            return Task.FromResult(Result.SuccessWithMessage("Swagger configuration updated successfully"));
+        }
+
+        protected override string GetDefaultErrorMessage()
+        {
+            return "An error occurred while updating the swagger configuration";
+        }
+    }
+}
