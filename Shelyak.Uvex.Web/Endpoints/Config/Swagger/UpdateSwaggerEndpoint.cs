4ce4672a0e1f768b808d71925711964a841cefc4
Julien Tschäppät <git@elendil.software>
2024-06-01 09:53:33 +0200

    Add Swagger configuration endpoint

---
+++ Shelyak.Uvex.Web/Endpoints/Config/Swagger/UpdateSwaggerEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using FastEndpoints;
+using Shelyak.Uvex.Web.Core.Settings;
+
+namespace Shelyak.Uvex.Web.Endpoints.Config.Swagger;
+
+public class UpdateSwaggerEndpoint : Endpoint<UpdateSwaggerRequest>
+{
+    internal const string RoutePattern = "/swagger";
+        
+    private readonly ISettingsUpdater _settingsUpdater;
+
+    public UpdateSwaggerEndpoint(ISettingsUpdater settingsUpdater)
+    {
+        _settingsUpdater = settingsUpdater;
+    }
+    
+    public override void Configure()
+    {
+        Put(RoutePattern);
+        Group<ConfigGroup>();
+        Version(1);
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(UpdateSwaggerRequest req, CancellationToken ct)
+    {
+        await _settingsUpdater.UpdateSwagger(req.Enabled);
+        await SendOkAsync(ct);
+    }
+}
---
+++ Shelyak.Uvex.Web/Endpoints/Config/Swagger/UpdateSwaggerRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Shelyak.Uvex.Web.Endpoints.Config.Swagger;
+
+public record UpdateSwaggerRequest(bool Enabled)
+{
+    
+    public const string Route = ConfigGroup.RoutePrefix + UpdateSwaggerEndpoint.RoutePattern;
+}
