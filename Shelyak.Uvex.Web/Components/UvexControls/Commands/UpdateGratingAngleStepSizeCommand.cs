b6634e132a0338a3a969ae0cd133ac5ff833f67e
Julien Tschäppät <git@elendil.software>
2024-06-06 21:31:48 +0200

    Memorize step sizes

---
+++ Shelyak.Uvex.Web/Components/UvexControls/Commands/UpdateGratingAngleStepSizeCommand.cs
@@ -0,0 +1,36 @@
+﻿using Ardalis.Result;
+using FastEndpoints;
+using Microsoft.Extensions.Localization;
+using Shelyak.Uvex.Web.Components.Shared.Commands;
+using Shelyak.Uvex.Web.Endpoints.Config.UvexControls;
+using Shelyak.Uvex.Web.Locales;
+
+namespace Shelyak.Uvex.Web.Components.UvexControls.Commands;
+
+public record UpdateGratingAngleStepSizeCommand(float StepSize) : ICommand<Result<EmptyResult>>
+{
+    public class Handler : CommandHandlerBase<UpdateGratingAngleStepSizeCommand, EmptyResult>
+    {
+        private readonly IStringLocalizer<LocalizationResources> _localizer;
+
+        public Handler(IHttpClientFactory httpClientFactory, IStringLocalizer<LocalizationResources> localizer) : base(httpClientFactory)
+        {
+            _localizer = localizer;
+        }
+
+        protected override async Task<HttpResponseMessage> ExecuteHttpRequest(UpdateGratingAngleStepSizeCommand command, CancellationToken ct)
+        {
+            return await HttpClient.PutAsJsonAsync(UpdateGratingAngleStepSizeRequest.Route, new UpdateGratingAngleStepSizeRequest(command.StepSize), ct);
+        }
+
+        protected override Task<Result<EmptyResult>> BuildSuccessResponse(HttpResponseMessage response, CancellationToken ct)
+        {
+            return Task.FromResult(Result.Success(EmptyResult.Instance));
+        }
+
+        protected override string GetDefaultErrorMessage()
+        {
+            return _localizer["Configuration_Focus_Config_StepSize_Command_Error"];
+        }
+    }
+}
--- Shelyak.Uvex.Web/Settings/UvexControlsSettings.cs
+++ Shelyak.Uvex.Web/Settings/UvexControlsSettings.cs
@@ -2,6 +2,8 @@
 
 public class UvexControlsSettings
 {
+    public const string SectionName = "UvexControls";
+    
     public float GratingAngleStepSize { get; set; }
     public float GratingWavelengthStepSize { get; set; }
     public float FocusStepSize { get; set; }
