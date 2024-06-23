bff2e4b26d0cd57df499b4249d664578f0f8cb2e
Julien Tschäppät <git@elendil.software>
2024-06-23 15:14:01 +0200

    Don't start browser during development

--- Shelyak.Uvex.Web/Configuration/BrowserStarter.cs
+++ Shelyak.Uvex.Web/Configuration/BrowserStarter.cs
@@ -6,10 +6,16 @@ public static class BrowserStarter
 {
     public static void StartBrowser(this WebApplicationBuilder builder, bool isStartedFromAscom)
     {
+        if (builder.Environment.IsDevelopment())
+        {
+            return;
+        }
+        
         if (!isStartedFromAscom)
         {
             Task.Run(async () =>
             {
+                //Delay to allow the server to start and avoid the browser to open too early
                 await Task.Delay(TimeSpan.FromSeconds(3));
 
                 Process.Start(new ProcessStartInfo
