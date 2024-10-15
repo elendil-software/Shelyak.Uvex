dced5d1fa0e2f1fb0d3b1bf6c2f44f45eb123df3
Julien Tschäppät <git@elendil.software>
2024-10-15 18:19:03 +0200

    Remove unused delegate
    From Qodana analysis

--- Shelyak.Uvex.Ascom/frmMain.cs
+++ Shelyak.Uvex.Ascom/frmMain.cs
@@ -4,8 +4,6 @@ namespace ASCOM.ShelyakUvex
 {
     public partial class FrmMain : Form
     {
-        private delegate void SetTextCallback(string text);
-
         public FrmMain()
         {
             InitializeComponent();
